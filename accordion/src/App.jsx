016d1e824e74d3446ed62008d484cb7ec1a24c8a
devisonsantana <devisonsantana@hotmail.com>
2026-08-13 14:50:46 -0300

    feat: implement accordion feature for each question and add react icons dependency

--- accordion/src/App.jsx
+++ accordion/src/App.jsx
@@ -1,9 +1,20 @@
-import Question from "./Question";
+import { useState } from "react";
+import SingleQuestion from "./Question";
+import data from "./data";
 
 export default function App() {
+  const [questions, setQuestions] = useState(data);
+
   return (
-    <>
-      <Question />
-    </>
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
   );
 }
