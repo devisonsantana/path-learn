cde0c76b954e43171c9a9391c6d2c01c765cd94d
devisonsantana <devisonsantana@hotmail.com>
2026-08-14 17:49:26 -0300

    feat: build the main page, add menu component and add images on public folder

--- menu/src/App.jsx
+++ menu/src/App.jsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+import Menu from "./Menu";
+import items from "./data";
+
 export default function App() {
-  return <></>;
+  const [menuItems, setMenuItems] = useState(items);
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline" />
+        </div>
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
