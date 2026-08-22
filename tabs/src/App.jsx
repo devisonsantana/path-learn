0b5c36cd7a00b29a132a9d6fa982d22998020b67
devisonsantana <devisonsantana@hotmail.com>
2026-08-21 21:45:53 -0300

    feat(tabs): add conditional rendering using state and fetch data with fallback

--- tabs/src/App.jsx
+++ tabs/src/App.jsx
@@ -1,3 +1,43 @@
+import { useEffect, useState } from "react";
+import data from "./data";
+
+const url = "https://course-api.com/react-tabs-project";
+
 export default function App() {
-  return <></>;
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const newJobs = await response.json();
+      setJobs(newJobs);
+    } catch {
+      setJobs(data);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>loading...</h1>
+      </section>
+    );
+  }
+
+  return (
+    <div>
+      {jobs.map((job) => {
+        return <div key={job.id}>{job.title}</div>;
+      })}
+    </div>
+  );
 }
