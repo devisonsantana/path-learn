c2738e56cabd9bc3c6bea98949f2268360d688f6
devisonsantana <devisonsantana@hotmail.com>
2026-08-26 20:16:10 -0300

    feat(task): add user input reader

--- task-tracker/Program.cs
+++ task-tracker/Program.cs
@@ -2,8 +2,49 @@
 
 public class Program
 {
-    public static void Main(string[] args)
+    static bool Running = true;
+    public static void Main()
     {
+        Console.WriteLine("[WELCOME MESSAGE]");
 
+        while (Running)
+        {
+            Console.Write("> ");
+            string? rawInput = Console.ReadLine()?.Trim();
+
+            if (string.IsNullOrWhiteSpace(rawInput))
+            {
+                continue;
+            }
+
+            string[] inputValues = rawInput.Split(' ', 2);
+            string command = inputValues[0].ToLower();
+
+            switch (command)
+            {
+
+                // TODO: OTHER COMMANDS HERE
+
+                case "help":
+                    Help();
+                    break;
+                case "exit":
+                    Exit();
+                    break;
+                default:
+                    Console.WriteLine($"Error: Command {command} not found...");
+                    break;
+            }
+        }
+    }
+    static void Help()
+    {
+        // TODO: LIST ALL COMMANDS
+        Console.WriteLine("[COMMANDS WILL BE HERE]");
+    }
+    static void Exit()
+    {
+        Console.WriteLine("Bye...");
+        Running = false;
     }
 }
