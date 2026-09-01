176df288449154d1bb8c5388881860837fb6a631
devisonsantana <devisonsantana@hotmail.com>
2026-09-01 01:45:33 -0300

    chore: remove dumb transactions

--- expense-tracker/src/context/GlobalState.jsx
+++ expense-tracker/src/context/GlobalState.jsx
@@ -2,13 +2,7 @@ import { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
 
 const initialState = {
-  transactions: [
-    { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: 300 },
-    { id: 3, text: "Book", amount: -10 },
-    { id: 4, text: "Camera", amount: 150 },
-    { id: 5, text: "Burger", amount: -15 },
-  ],
+  transactions: [],
 };
 
 export const GlobalContext = createContext(initialState);
