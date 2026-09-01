6498a60dc3b383f6f170db9a1010e0029a1fcf0e
devisonsantana <devisonsantana@hotmail.com>
2026-08-31 22:13:25 -0300

    chore: add balance and income expenses components

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -1,9 +1,15 @@
+import { Balance } from "./components/Balance";
 import { Header } from "./components/Header";
+import { IncomeExpenses } from "./components/IncomeExpenses";
 
 export default function App() {
   return (
     <>
       <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
     </>
   );
 }
---
+++ expense-tracker/src/components/Balance.jsx
@@ -0,0 +1,8 @@
+export const Balance = () => {
+  return (
+    <>
+      <h4>Balance</h4>
+      <h1 id="balance">$0.00</h1>
+    </>
+  );
+};
