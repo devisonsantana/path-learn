d3d5468b0eaee2de037d3388fbc3a45571a2d5d6
devisonsantana <devisonsantana@hotmail.com>
2026-09-10 11:26:28 -0300

    feat(grocery): set component structure

--- grocery-bud/src/App.jsx
+++ grocery-bud/src/App.jsx
@@ -3,7 +3,40 @@ import Alert from "./Alert";
 import List from "./List";
 
 function App() {
-  return <div>App</div>;
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="section-center">
+      <div className="grocery-container">
+        <form className="grocery-form" onSubmit={handleSubmit}>
+          {alert.show && <Alert />}
+          <h3>grocery bud</h3>
+          <div className="form-control">
+            <input
+              type="text"
+              className="grocery"
+              placeholder="e.g eggs"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button type="submit" className="submit-btn">
+              {isEditing ? "edit" : "submit"}
+            </button>
+          </div>
+        </form>
+        <List />
+        <button className="clear-btn">clear items</button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
