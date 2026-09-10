0d60ce93a098aa6ec8b6c48f824a042db738b33f
devisonsantana <devisonsantana@hotmail.com>
2026-09-10 11:08:36 -0300

    chore(grocery): add initial project structure

---
+++ grocery-bud/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import Alert from "./Alert";
+import List from "./List";
+
+function App() {
+  return <div>App</div>;
+}
+
+export default App;
---
+++ grocery-bud/src/List.jsx
@@ -0,0 +1,7 @@
+import { FaEdit, FaTrash } from "react-icons/fa";
+
+function List() {
+  return <div>List</div>;
+}
+
+export default List;
