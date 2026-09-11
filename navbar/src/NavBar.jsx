05dd53762824051b4ebdd2d4da2a430868347ddf
devisonsantana <devisonsantana@hotmail.com>
2026-09-11 12:29:59 -0300

    feat(navbar): implement navigation toggle bar using useRef and css transitions

--- navbar/src/NavBar.jsx
+++ navbar/src/NavBar.jsx
@@ -4,17 +4,31 @@ import { links, social } from "./data";
 import logo from "./assets/logo.svg";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  useEffect(() => {
+    const { height } = linksRef.current.getBoundingClientRect();
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${height}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className="nav-toggle">
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map(({ id, url, text }) => (
               <li key={id}>
                 <a href={url}>{text}</a>
