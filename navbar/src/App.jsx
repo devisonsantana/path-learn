8e3cb001e2cb3a42b9df4633be717b5239b3250b
devisonsantana <devisonsantana@hotmail.com>
2026-09-11 11:58:35 -0300

    feat(navbar): add navbar with dynamic links

--- navbar/src/App.jsx
+++ navbar/src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from "./NavBar";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default App;
--- navbar/src/NavBar.jsx
+++ navbar/src/NavBar.jsx
@@ -4,7 +4,34 @@ import { links, social } from "./data";
 import logo from "./assets/logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>;
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container show-container">
+          <ul className="links">
+            {links.map(({ id, url, text }) => (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map(({ id, url, icon }) => (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
